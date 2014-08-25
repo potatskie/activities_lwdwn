bc4f3c3dece1088ff34639f65fb7819e1c5e87b8
ucommerzdev3 <ucommerzdev3@ucommerz.com>
2014-08-25 19:00:38 +0800

    custom field for the post page full width image

--- wp-content/themes/wpex-fashionista/formats/single-standard.php
+++ wp-content/themes/wpex-fashionista/formats/single-standard.php
@@ -1,9 +1,10 @@
 <?php
-/**
- * @package WordPress
- * @subpackage WPEX WordPress Framework
- * Standard Post Format
- */
+$fullwidthimg_arr = simple_fields_value('full_width_image');
 
-//show featured image if available
-wpex_post_thumbnail(); ?>
+if($fullwidthimg_arr && $fullwidthimg_arr['is_image']){
+	echo '<img src="' . $fullwidthimg_arr['image_src']['full'][0] . '" alt="' . get_the_title() .'" />';
+}
+else{
+	wpex_post_thumbnail(); 
+}
+?>
