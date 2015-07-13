9613a61475f08d5b3e0d9a6e1be552a1ba98810b
ucommerzdev3 <ucommerzdev3@ucommerz.com>
2015-07-13 14:57:49 +0800

    tweaks july 13

--- wp-content/themes/blink/partials/page-cover.php
+++ wp-content/themes/blink/partials/page-cover.php
@@ -7,19 +7,10 @@ $hide_meta = (bool) get_theme_mod( 'layout-page-meta', blink_get_default( 'layou
 ?>
 
 <section class="page-cover">
-	<div class="overlay"></div>
-
 	<div class="page-cover-inside inner-block">
 		<header class="entry-header">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</header><!-- .entry-header -->
 
-		<?php if ( ! $hide_meta ) : ?>
-		<footer class="entry-footer">
-			<?php blink_posted_on(); ?>
-
-			<?php blink_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-		<?php endif; ?>
 	</div><!-- .page-cover-inside -->
 </section><!-- .page-cover -->
